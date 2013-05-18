bf4910fd676dac09eb28f131cded390586fed0d0
Mathijs Saey <mathsaey@vub.ac.be>
2013-05-18 22:07:36 +0200

    Calling correct at file from within the WeScrabbleActivity, removed all bin and gen files from the repo

--- weScrabble/weScrabble/src/soft/vub/weScrabble/Activities/WeScrabble.java
+++ weScrabble/weScrabble/src/soft/vub/weScrabble/Activities/WeScrabble.java
@@ -1,84 +1,73 @@
-/**
- * AmbientTalk/2 Project
- * (c) Software Languages Lab, 2006 - 2011
- * Authors: Software Languages Lab - Ambient Group
- * 
- * Permission is hereby granted, free of charge, to any person
- * obtaining a copy of this software and associated documentation
- * files (the "Software"), to deal in the Software without
- * restriction, including without limitation the rights to use,
- * copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the
- * Software is furnished to do so, subject to the following
- * conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
- * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package soft.vub.weScrabble.Activities;
 
 import soft.vub.weScrabble.R;
 import soft.vub.weScrabble.WeScrabbleAssetInstaller;
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import edu.vub.at.IAT;
 import edu.vub.at.android.util.IATAndroid;
 import edu.vub.at.android.util.IATSettings;
 import edu.vub.at.android.util.IATSettings.IATOptions;
 
 public class WeScrabble extends Activity {
-
 	private static IAT iat;
-	public static Handler mHandler;
-	private static final int _ASSET_INSTALLER_ = 0;
 	
-	//StartIATTask based on WeScribble.java from lab sessions
+	private TextView progressText;
+	private ProgressBar progressSpinner;
+	
 	public class StartIATTask extends AsyncTask<Void, String, Void> {
-		private ProgressDialog pd;
 
+		@Override
 		protected void onProgressUpdate(String... values) {
 			super.onProgressUpdate(values);
-			pd.setMessage(values[0]);
-		}
-
-		protected void onPreExecute() {
-			super.onPreExecute();
-			pd = ProgressDialog.show(WeScrabble.this, "weScrabble", "Loading AmbientTalk...");
-		}
-		
-		protected void onPostExecute(Void result) {
-			super.onPostExecute(result);
-			pd.dismiss();
+			progressText.setText(values[0]);
 		}
 
 		@Override
-		protected Void doInBackground(Void... arg0) {
+		protected void onPreExecute() {
+			super.onPreExecute();
+			progressText.setText("Loading...");
+		}
+		
+		@Override
+		protected void onPostExecute(Void result) {
+			super.onPostExecute(result);
+			progressText.setText("Done!");
+			progressSpinner.setVisibility(View.INVISIBLE);
+		}
+
+		@Override
+		protected Void doInBackground(Void... arg0) {			
 			try {
-				IATOptions iatOptions = IATSettings.getIATOptions(WeScrabble.this);
-				iatOptions.networkName_ = "AmbientTalk"; //TODO
+				Looper.prepare();
 				
+				//Install the ambienttalk assets
+				this.publishProgress("Installing AmbientTalk Assets");
+				
+				if (iat == null) {
+					WeScrabbleAssetInstaller installer = new WeScrabbleAssetInstaller();
+					installer.launch(WeScrabble.this);
+				}
+				
+				//Launch the interpreter
+				this.publishProgress("Starting the AmbientTalk interpreter");
+				
+				IATOptions iatOptions = IATSettings.getIATOptions(WeScrabble.this);				
 				iat = IATAndroid.create(WeScrabble.this, iatOptions);
 				
+				//Start code evaluation
 				this.publishProgress("Loading weScrabble code");
-				iat.evalAndPrint("import /.demo.weScribble.weScribble.makeWeScribble()", System.err);
+				iat.evalAndPrint("import /.weScrabble.atTest.test()", System.err);
 				
 			} catch (Exception e) {
+				this.publishProgress("Failed to load ambientTalk...");
 				Log.e("AmbientTalk", "Could not start IAT", e);
 			}
 			return null;
@@ -87,31 +76,17 @@ public class WeScrabble extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		StartIATTask task;
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (iat == null) {
-			Intent i = new Intent(this, WeScrabbleAssetInstaller.class);
-			startActivityForResult(i, _ASSET_INSTALLER_);
-		}
+		progressText = (TextView) findViewById(R.id.progressText);
+		progressSpinner = (ProgressBar) findViewById(R.id.progressBar);
+		
+		task = new StartIATTask();	
+		task.execute();
 	}
 
-	/*
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		Log.v("WeScribble", "Return of Asset Installer activity");
-		switch (requestCode) {
-		case (_ASSET_INSTALLER_):
-			if (resultCode == Activity.RESULT_OK) {
-				Thread lt = new Tre();
-				lt.start();
-				mHandler = lt.mHandler;
-				new StartIATTask().execute((Void)null);
-			}
-			break;
-		}
-	}
-	*/
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- weScrabble/weScrabble/src/soft/vub/weScrabble/WeScrabbleAssetInstaller.java
+++ weScrabble/weScrabble/src/soft/vub/weScrabble/WeScrabbleAssetInstaller.java
@@ -1,7 +1,18 @@
 package soft.vub.weScrabble;
 
+import android.app.Activity;
 import edu.vub.at.android.util.AssetInstaller;
 
-//Allows us to override the AT asset installer in case we need to make modifications
 public class WeScrabbleAssetInstaller extends AssetInstaller {
+	
+	//Ensures the assets are copied to the device
+	public WeScrabbleAssetInstaller() {
+		super();
+	}
+	
+	//Installs the assets, this avoids having to use an intent to do this
+	public void launch(Activity a) {
+		copyDefaultAssets(a);
+		copyAssets(a);
+	}
 }
